df0a704eb0c6d7714ced0b6c90c8dac4ea438598
mokulling <72229978+mokulling@users.noreply.github.com>
2021-01-17 18:08:54 -0600

    added in badge links

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const fs = require('fs')
 const questions = ['what is the title of your project?', 'What is the description of the project?', 'What license would you like for your application?', 'what is your github username?', 'what is your email address?'
 ];
 
- 
+const cc = '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)'
+const mit = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+const apache = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+const gnu = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)' 
 
 
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(fileName, data, badgeLink) {
    
     const inputs = [`${data.title}`, `${data.description}`, `${data.license}`, `${data.github}`, `${data.email}`]
         const filename = `${data.title.toLowerCase().split(' ').join('')}.MD`;
-        fs.writeFile(filename, '\n\n## Description' +
+        fs.writeFile(filename, badgeLink + '\n\n## Description' +
         '\n\n'+ inputs[1] + '\n\n# Table of Contents \n\n' + '1. [Description](#Description)\n 2. [Questions](#Questions?) \n\n 3. [Installation](#Installation) \n\n 4. [Usage](#Usage) \n\n 5. [Contributing](#Contributing) \n\n6. [License](#License) \n\n 7. [Tests](#Tests)', (err) =>{
     if (err) throw (err)        
         }
@@ -47,7 +50,7 @@ function init() {
         {
         type: 'list',
         message: questions[2],
-        choices: ['Academic', 'CC', 'MIT', 'GNU'],
+        choices: ['Apache', 'CC', 'MIT', 'GNU'],
         name: 'license'
             } ,
 
@@ -66,7 +69,14 @@ function init() {
 
     ])
     .then((data) => {
-        writeToFile(`${data.title.toLowerCase().split(' ').join('')}.MD`, data)
+        if (`${data.license}` =='CC') {
+            badgeLink = cc
+        } else if (`${data.license}` == 'Apache') {
+            badgeLink = apache
+        } else if (`${data.license}` == 'GNU') {
+            badgeLink = gnu
+        } else badgeLink = mit
+        writeToFile(`${data.title.toLowerCase().split(' ').join('')}.MD`, data, badgeLink)
 
     })}
 
