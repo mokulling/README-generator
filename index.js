12647702a3b88e293c8f99d728b70bfd0501026d
mokulling <72229978+mokulling@users.noreply.github.com>
2021-01-17 18:01:03 -0600

    creates everything except badge

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const fs = require('fs')
 const questions = ['what is the title of your project?', 'What is the description of the project?', 'What license would you like for your application?', 'what is your github username?', 'what is your email address?'
 ];
 
+ 
+
+
 
 // function to write README file
 function writeToFile(fileName, data) {
-        const inputs = [`${data.title}`, `${data.description}`, `${data.license}`, `${data.github}`, `${data.email}`]
-        
-        console.log(inputs)
+   
+    const inputs = [`${data.title}`, `${data.description}`, `${data.license}`, `${data.github}`, `${data.email}`]
         const filename = `${data.title.toLowerCase().split(' ').join('')}.MD`;
         fs.writeFile(filename, '\n\n## Description' +
         '\n\n'+ inputs[1] + '\n\n# Table of Contents \n\n' + '1. [Description](#Description)\n 2. [Questions](#Questions?) \n\n 3. [Installation](#Installation) \n\n 4. [Usage](#Usage) \n\n 5. [Contributing](#Contributing) \n\n6. [License](#License) \n\n 7. [Tests](#Tests)', (err) =>{
@@ -22,14 +24,11 @@ function writeToFile(fileName, data) {
             '## Questions?\n\n\n' +
              'Contact me here:\n\n' + 
              '[GitHub Profile](https://www.github.com/' + inputs[3] + ')' + 
-            '\n\n[Email](mailto:'+inputs[4] +')', 
+            '\n\n[Email](mailto:'+inputs[4] +')' + '\n\n Reach out to me at either of the links above if you have any questions regarding the project or if you want to become a collaborator.', 
              (err) => console.log(err))
-            
-            
-
-
+             
       };
-
+    
 
 // function to initialize program
 function init() {
@@ -67,8 +66,19 @@ function init() {
 
     ])
     .then((data) => {
-    writeToFile(`${data.title.toLowerCase().split(' ').join('')}.MD`, data)
+        writeToFile(`${data.title.toLowerCase().split(' ').join('')}.MD`, data)
 
     })}
+
+        
+        
+    
+    
+    
+
+
+
+
+    
     // function call to initialize program
 init()
